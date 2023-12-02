992dbc0225640b6bd39dadd59ed3b8dfe1ea16a3
morrillchelsea <151207969+morrillchelsea@users.noreply.github.com>
2023-12-02 15:46:10 -0600

    Modified loghander to correct erroneous db_logger name (meerkat was from the reference) and added comments

--- FortuneTeller/LogHandler.py
+++ FortuneTeller/LogHandler.py
@@ -7,30 +7,42 @@ import logging
 from logging import FileHandler
 from logging import Formatter
 
-#Create and modify logger
+# Create format for logged message
 log_format = (
 "%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno)d")
+# Assign default logging level
 log_level = logging.INFO
 
-# messaging logger
+# User Logger
+# create pathname to store log file
 user_log_file = "./user_log.log"
-
+# Create logger
 user_logger = logging.getLogger("fortuneteller.user")
+# Assign default log level to logger
 user_logger.setLevel(log_level)
+# Create file handler
 user_logger_FH = FileHandler(user_log_file)
+# Assign log level to file handler
 user_logger_FH.setLevel(log_level)
+# Apply formatting to file handler
 user_logger_FH.setFormatter(Formatter(log_format))
-# add handler to user_logger
+# Add handler to user_logger
 user_logger.addHandler(user_logger_FH)
 
-# payments logger
+# DB Logger
+# Create pathname to store log file
 db_log_file = "./db_log.log"
-db_logger = logging.getLogger("wasted_meerkats.payments")
-
+# Create logger
+db_logger = logging.getLogger("fortuneteller.db")
+# Set level to logger
 db_logger.setLevel(log_level)
+# Create file handler
 db_logger_FH = FileHandler(db_log_file)
+# Set level to file handler
 db_logger_FH.setLevel(log_level)
+# Apply formatting to file handler
 db_logger_FH.setFormatter(Formatter(log_format))
+# Add handler to db_logger
 db_logger.addHandler(db_logger_FH)
     
     
