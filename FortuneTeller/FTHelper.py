ccaf6742eb0c591b4e3f572ce4edbb2c36f73584
hoilamwong <142138307+hoilamwong@users.noreply.github.com>
2023-12-04 15:04:36 -0500

    Main Menu can now change to display different options when user is logged in.
    Changes the format of text documents
    Updated file parsing
    
    Added Logging to reading text file (Chelsea)

--- FortuneTeller/FTHelper.py
+++ FortuneTeller/FTHelper.py
@@ -2,6 +2,12 @@ from tkinter import *
 import random
 from LogHandler import db_logger
 
+LOVE_FORTUNE_PATH = "texts/love_fortune.txt"
+CAREER_FORTUNE_PATH = "texts/career_fortune.txt"
+GENERAL_FORTUNE_PATH = "texts/general_fortune.txt"
+HEALTH_FORTUNE_PATH = "texts/health_fortune.txt"
+
+
 def center_window(win):
     """
         centers a tkinter window
@@ -50,57 +56,84 @@ def crystal_ball_ascii_art(win):
     crystal_ball_text = pad_to_center(crystal_ball_raw.split('\n'), 60)
     Label(win, justify=LEFT, text=crystal_ball_text).pack()
 
+
 def get_love_fortune():
     try:
-        with open('texts/love_fortune.txt', 'r', encoding = 'utf-8') as file:
-            all_text: str = file.read()
-            fortune = list(map(str, all_text.split(':')))
-            # Print random fortune from love_fortune.txt
-            return random.choice(fortune)
+        fortune = []
+        file1 = open(LOVE_FORTUNE_PATH, 'r')
+        lines = file1.read().splitlines()
+
+        for line in lines:
+            fortune.append(line)
+        return random.choice(fortune)
     except IOError as err:
         print('Unable to find file love_fortune.txt')
         db_logger.error(err)
 
+
 def get_career_fortune():
     try:
-        with open('texts/career_fortune.txt', 'r', encoding = 'utf-8') as file:
-            all_text: str = file.read()
-            fortune = list(map(str, all_text.split(':')))
-            # Print random fortune from career_fortune.txt
-            return random.choice(fortune)
+        fortune = []
+        file1 = open(CAREER_FORTUNE_PATH, 'r')
+        lines = file1.read().splitlines()
+
+        for line in lines:
+            fortune.append(line)
+        return random.choice(fortune)
     except IOError as err:
         print('Unable to find file career_fortune.txt')
         db_logger.error(err)
 
+
 def get_health_fortune():
     try:
-        with open('texts/health_fortune.txt', 'r', encoding = 'utf-8') as file:
-            all_text: str = file.read()
-            fortune = list(map(str, all_text.split(':')))
-            # Print random fortune from health_fortune.txt
-            return random.choice(fortune)
+        fortune = []
+        file1 = open(HEALTH_FORTUNE_PATH, 'r')
+        lines = file1.read().splitlines()
+
+        for line in lines:
+            fortune.append(line)
+        return random.choice(fortune)
     except IOError as err:
         print('Unable to find file health_fortune.txt')
         db_logger.error(err)
 
+
 def get_general_fortune():
     try:
-        with open('texts/general_fortune.txt', 'r', encoding = 'utf-8') as file:
-            all_text: str = file.read()
-            fortune = list(map(str, all_text.split(':')))
-            # Print random fortune from general_fortune.txt
-            return random.choice(fortune)
+        fortune = []
+        file1 = open(GENERAL_FORTUNE_PATH, 'r')
+        lines = file1.read().splitlines()
+
+        for line in lines:
+            fortune.append(line)
+        return random.choice(fortune)
     except IOError as err:
         print('Unable to find file general_fortune.txt')
         db_logger.error(err)
 
+
 def get_random_fortune():
+    fortune = []
     try:
-        with fileinput.input(
-                files=("love_fortune.txt", "career_fortune.txt", "health_fortune.txt", "general_fortune.txt")) as file:
-            all_text: str = file.read()
-            fortune = list(map(str, all_text.split(":")))
-            # Print random fortune from any of the .txt files
+        # Open multiple text files
+        with open(LOVE_FORTUNE_PATH, 'r') as file_love, open(GENERAL_FORTUNE_PATH, 'r') as file_general, open(
+                HEALTH_FORTUNE_PATH, 'r') as file_health, open(CAREER_FORTUNE_PATH, 'r') as file_career:
+            # Read text files and split by line
+            lines_love = file_love.read().splitlines()
+            lines_general = file_general.read().splitlines()
+            lines_health = file_health.read().splitlines()
+            lines_career = file_career.read().splitlines()
+
+            # Append lines to fortune
+            for line in lines_love:
+                fortune.append(line)
+            for line in lines_general:
+                fortune.append(line)
+            for line in lines_health:
+                fortune.append(line)
+            for line in lines_career:
+                fortune.append(line)
             return random.choice(fortune)
     except IOError as err:
         db_logger.error(err)
